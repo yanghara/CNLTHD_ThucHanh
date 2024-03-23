9430da4f7301da137fe22c2ccbbff3ea49cd2267
yanghara <109479069+yanghara@users.noreply.github.com>
2024-03-23 21:21:55 +0700

    ngay dau

--- ecourseapiv4/courses/models.py
+++ ecourseapiv4/courses/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from ckeditor.fields import RichTextField
 from cloudinary.models import CloudinaryField
+
 class User(AbstractUser):
-    pass
+    avatar = CloudinaryField(null=True)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-
+    icon = models.CharField(max_length=50, default='tag')
     def __str__(self):
         return self.name
 
@@ -20,7 +21,7 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
-
+        ordering = ['-id']
 
 class Course(BaseModel):
     name = models.CharField(max_length=255)
@@ -31,12 +32,31 @@ class Course(BaseModel):
     def __str__(self):
         return self.name
 
+class Tag(BaseModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Lesson(BaseModel):
     subject = models.CharField(max_length=255)
     content = RichTextField()
     image = CloudinaryField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
+    tags = models.ManyToManyField(Tag)
     class Meta:
         unique_together = ('subject', 'course')
+
+class Interaction(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+class Comment(Interaction):
+    content = models.CharField(max_length=500)
+
+class Like(Interaction):
+    class Meta:
+        unique_together = ('lesson', 'user')
